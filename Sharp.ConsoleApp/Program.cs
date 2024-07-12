e8a098c5f1444dbcffbd85de88d7b63f03cac909
RadagastW <radagastw@yandex.ru>
2024-07-12 13:02:58 +0500

    add: Sharp.ConsoleApp project files

---
+++ Sharp.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Sharp.ConsoleApp
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Основная точка входа в приложение.
+        /// </summary>
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
