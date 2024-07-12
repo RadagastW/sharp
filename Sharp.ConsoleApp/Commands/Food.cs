48f1ce4fda94010ccdf9482d3baf021a91656d52
RadagastW <radagastw@yandex.ru>
2024-07-12 14:02:27 +0500

    feat: implement of Food class

---
+++ Sharp.ConsoleApp/Commands/Food.cs
@@ -0,0 +1,30 @@
+﻿using Sharp.ConsoleApp.Interfaces;
+using Sharp.ConsoleApp.Utilities;
+using System;
+
+namespace Sharp.ConsoleApp.Commands
+{
+    /// <summary>
+    /// Команда для вычисления возможности покупки еды за монеты.
+    /// </summary>
+    public class Food : ICommand
+    {
+        private const int FOOD_UNIT_PRICE = 10;
+
+        /// <summary>
+        /// Выполнить команду вычисления возможности покупки еды за монеты.
+        /// </summary>
+        public void Execute()
+        {
+            Console.WriteLine($"Добро пожаловать в пекарню. Сегодня еда по {FOOD_UNIT_PRICE} монет.");
+            int money = ConsoleUtilites.ReadIntFromConsole("Сколько у вас золота? ");
+            int food = ConsoleUtilites.ReadIntFromConsole("Сколько еды вам нужно? ");
+
+            bool isAbleToPay = money >= food * FOOD_UNIT_PRICE;
+            food *= Convert.ToInt32(isAbleToPay);
+            money -= food * FOOD_UNIT_PRICE;
+
+            Console.WriteLine($"У вас в сумке {food} едениц еды и {money} монет.");
+        }
+    }
+}
