063f2747b53d98240105cab80e82e400bb054e39
RadagastW <radagastw@yandex.ru>
2024-07-12 13:07:00 +0500

    feat: implementation of HealthCalculation class

---
+++ Sharp.ConsoleApp/Commands/HealthCalculation.cs
@@ -0,0 +1,54 @@
+﻿using Sharp.ConsoleApp.Interfaces;
+using System;
+
+namespace Sharp.ConsoleApp.Commands
+{
+    /// <summary>
+    /// Команда для вычисления оставшегося здоровья после удара.
+    /// </summary>
+    public class HealthCalculation : ICommand
+    {
+        private const int PERCENT_CONVERTER = 100;
+
+        /// <summary>
+        /// Выполнить команду вычисления оставшегося здоровья после удара.
+        /// </summary>
+        public void Execute()
+        {
+            int health = ReadIntFromConsole("Введите кол-во здоровья");
+            int armor = ReadIntFromConsole("Введите кол-во брони");
+            int damage = ReadIntFromConsole("Введите кол-во урона");
+
+            health -= damage * armor / PERCENT_CONVERTER;
+
+            Console.WriteLine($"Вам нанесли {damage} урона. У вас осталось {health} здоровья.");
+        }
+
+        /// <summary>
+        /// Запрашивает у пользователя ввод целого числа. Повторяет запрос до тех пор, пока не будет введено корректное значение.
+        /// </summary>
+        /// <param name="prompt">Сообщение для запроса ввода, отображаемое пользователю.</param>
+        /// <returns>Целочисленное значение, введенное пользователем.</returns>
+        private int ReadIntFromConsole(string prompt)
+        {
+            int value;
+
+            while (true)
+            {
+                Console.Write($"{prompt}: ");
+
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out value))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод. Пожалуйста, введите правильное число.");
+                }
+            }
+
+            return value;
+        }
+    }
+}
