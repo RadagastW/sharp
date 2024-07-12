5b674394360e8f147bf1bba4124e94aeebd6113f
RadagastW <radagastw@yandex.ru>
2024-07-12 13:04:38 +0500

    feat: added code for the main logic of the program and the implementation of Exit and Help commands

---
+++ Sharp.ConsoleApp/Interfaces/ICommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Sharp.ConsoleApp.Interfaces
+{
+    /// <summary>
+    /// Интерфейс для выполнения команды.
+    /// </summary>
+    public interface ICommand
+    {
+        /// <summary>
+        /// Выполнить команду.
+        /// </summary>
+        void Execute();
+    }
+}
--- Sharp.ConsoleApp/Program.cs
+++ Sharp.ConsoleApp/Program.cs
@@ -1,13 +1,32 @@
-﻿namespace Sharp.ConsoleApp
+﻿using Sharp.ConsoleApp.Configuration;
+using Sharp.ConsoleApp.Interfaces;
+using System;
+
+namespace Sharp.ConsoleApp
 {
     internal class Program
     {
         /// <summary>
         /// Основная точка входа в приложение.
         /// </summary>
-        static void Main(string[] args)
+        static void Main()
         {
+            string input = "";
 
+            while (input != "EXIT")
+            {
+                Console.Write("Введите команду: ");
+                input = Console.ReadLine()?.ToUpper();
+
+                if (input != null && CommandFactory.GetCommands().TryGetValue(input, out ICommand command))
+                {
+                    command.Execute();
+                }
+                else
+                {
+                    Console.WriteLine("Неизвестная команда. Введите HELP для получения списка команд.");
+                }
+            }
         }
     }
 }
