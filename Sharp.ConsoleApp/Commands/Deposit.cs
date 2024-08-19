d1a8536a55b08cbeb5305004f41aaf3b35e58d4f
RadagastW <radagastw@yandex.ru>
2024-08-19 15:19:34 +0500

    feat: implementation of Deposit class

---
+++ Sharp.ConsoleApp/Commands/Deposit.cs
@@ -0,0 +1,28 @@
+﻿using Sharp.ConsoleApp.Interfaces;
+using Sharp.ConsoleApp.Utilities;
+using System;
+
+namespace Sharp.ConsoleApp.Commands
+{
+    /// <summary>
+    /// Команда для расчета депозита.
+    /// </summary>
+    public class Deposit : ICommand
+    {
+        /// <summary>
+        /// Выполнить команду для расчета депозита.
+        /// </summary>
+        public void Execute()
+        {
+            float money = ConsoleUtilites.ReadFromConsole<float>("Введите количество денежных средств на счету: ");
+            int years = ConsoleUtilites.ReadFromConsole<int>("На сколько лет открыт вклад: ");
+            float persent = ConsoleUtilites.ReadFromConsole<float>("Под какой процент открыт вклад: ");
+
+            for (int i = 0; i < years; i++)
+            {
+                money += money * persent / 100;
+                Console.WriteLine($"В {i} году у вас {money} денежных средств.");
+            }
+        }
+    }
+}
