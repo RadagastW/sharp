8a454af55fdbbaf996df95588388452617a2bb64
RadagastW <radagastw@yandex.ru>
2024-08-21 12:22:54 +0500

    feat: implementation of Registration class for passenger check-in

---
+++ Sharp.ConsoleApp/Commands/Registration.cs
@@ -0,0 +1,80 @@
+﻿using Sharp.ConsoleApp.Interfaces;
+using Sharp.ConsoleApp.Utilities;
+using System;
+
+namespace Sharp.ConsoleApp.Commands
+{
+    /// <summary>
+    /// Команда для регистрации пассажиров на рейс.
+    /// </summary>
+    public class Registration : ICommand
+    {
+        private readonly int[] _sectors = { 6, 28, 15, 15, 17 };
+
+        /// <summary>
+        /// Выполнить команду для регистрации пассажиров на рейс.
+        /// </summary>
+        public void Execute()
+        {
+            while (true)
+            {
+                DisplaySectors();
+                DisplayMenu();
+
+                switch (ConsoleUtilites.ReadFromConsole<int>("Введите номер команды: "))
+                {
+                    case 1:
+                        BookSeats();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        return;
+                    default:
+                        Console.WriteLine("Неверная команда.");
+                        break;
+                }
+
+                Console.ReadKey();
+            }
+        }
+
+        private void DisplaySectors()
+        {
+            Console.Clear();
+            Console.SetCursorPosition(0, 20);
+            for (int i = 0; i < _sectors.Length; i++)
+            {
+                Console.WriteLine($"В секторе {i + 1} свободно {_sectors[i]} мест.");
+            }
+            Console.SetCursorPosition(0, 0);
+        }
+
+        private void DisplayMenu()
+        {
+            Console.WriteLine("Регистрации пассажиров на рейс.");
+            Console.WriteLine("\n\n1 - забронировать места\n\n2 - выход из программы\n\n");
+        }
+
+        private void BookSeats()
+        {
+            int userSector = ConsoleUtilites.ReadFromConsole<int>("Сектор: ");
+
+            if (userSector < 1 || userSector > _sectors.Length)
+            {
+                Console.WriteLine("Неверный сектор.");
+                return;
+            }
+
+            int userPlaceAmount = ConsoleUtilites.ReadFromConsole<int>("Количество мест: ");
+
+            if (userPlaceAmount < 1 || userPlaceAmount > _sectors[userSector - 1])
+            {
+                Console.WriteLine("Неверное количество мест.");
+                return;
+            }
+
+            _sectors[userSector - 1] -= userPlaceAmount;
+            Console.WriteLine("Места забронированы.");
+        }
+    }
+}
