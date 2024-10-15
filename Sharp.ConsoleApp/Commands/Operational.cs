bac1091fe6dabddf8928753c868fc70756e6b4c0
RadagastW <radagastw@yandex.ru>
2024-10-15 14:00:39 +0500

    feat: implement of Operational class as template class

---
+++ Sharp.ConsoleApp/Commands/Operational.cs
@@ -0,0 +1,18 @@
+﻿using Sharp.ConsoleApp.Interfaces;
+
+namespace Sharp.ConsoleApp.Commands
+{
+    /// <summary>
+    /// Команда для...
+    /// </summary>
+    public class Operational : ICommand
+    {
+        /// <summary>
+        /// Выполнить команду для...
+        /// </summary>
+        public void Execute()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
