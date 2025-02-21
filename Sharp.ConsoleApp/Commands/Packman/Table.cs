9596f2ed351b0dcccae0df61e00dc292c414bd89
RadagastW <radagastw@yandex.ru>
2025-02-21 16:55:31 +0500

    feat: implement of Booking class

---
+++ Sharp.ConsoleApp/Commands/Packman/Table.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Sharp.ConsoleApp.Commands.Packman
+{
+    public class Table
+    {
+        public int Number;
+        public int MaxPlaces;
+        public int FreePlaces;
+
+        public Table(int number, int maxPlaces)
+        {
+            Number = number;
+            MaxPlaces = maxPlaces;
+            FreePlaces = maxPlaces;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Стол: {Number}. Свободных мест: {FreePlaces} из {MaxPlaces}.");
+        }
+
+        public bool Reserve(int plases)
+        {
+            if (FreePlaces >= plases)
+            {
+                FreePlaces -= plases;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
