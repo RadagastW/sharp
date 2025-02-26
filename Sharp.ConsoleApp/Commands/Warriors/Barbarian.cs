8fdb9d539d670a443da5b65cca6c514b994a44ce
RadagastW <radagastw@yandex.ru>
2025-02-26 14:32:49 +0500

    feat: implementation of Warriors class

---
+++ Sharp.ConsoleApp/Commands/Warriors/Barbarian.cs
@@ -0,0 +1,17 @@
+﻿namespace Sharp.ConsoleApp.Commands.Warriors
+{
+    public class Barbarian : Warrior
+    {
+        public Barbarian(int health, int armor, int damage, int attackSpeed)
+            : base(health, armor, damage * attackSpeed)
+        {
+            Name = "Варвар";
+        }
+
+        public void Shout()
+        {
+            Armor -= 2;
+            Health += 10;
+        }
+    }
+}
---
+++ Sharp.ConsoleApp/Commands/Warriors/Knight.cs
@@ -0,0 +1,16 @@
+﻿namespace Sharp.ConsoleApp.Commands.Warriors
+{
+    public class Knight : Warrior
+    {
+        public Knight(int health, int damage)
+            : base(health, 5, damage)
+        {
+            Name = "Рыцарь";
+        }
+
+        public void Pray()
+        {
+            Armor += 2;
+        }
+    }
+}
