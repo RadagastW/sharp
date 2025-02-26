3d14f19ee99610ef38ea2c5670517a29f6da8ae5
RadagastW <radagastw@yandex.ru>
2025-02-26 15:34:21 +0500

    feat: implement of Render class

---
+++ Sharp.ConsoleApp/Commands/Render/Player.cs
@@ -0,0 +1,14 @@
+﻿namespace Sharp.ConsoleApp.Commands.Render
+{
+    public class Player
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Player(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ Sharp.ConsoleApp/Commands/Render/Render.cs
@@ -0,0 +1,26 @@
+﻿using Sharp.ConsoleApp.Interfaces;
+using System;
+
+namespace Sharp.ConsoleApp.Commands.Render
+{
+    /// <summary>
+    /// Команда для Render.
+    /// </summary>
+    public class Render : ICommand
+    {
+        /// <summary>
+        /// Выполнить команду для Render.
+        /// </summary>
+        public void Execute()
+        {
+            Renderer renderer = new Renderer();
+            Player player = new Player(50, 0);
+
+            renderer.Draw(player.X, player.Y);
+
+            Console.WriteLine("Нажмите любую клавишу для продолжения...");
+            Console.ReadKey();
+            Console.WriteLine();
+        }
+    }
+}
