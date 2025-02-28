79f761786d7aadc0d4ebee64ee6239bbc5f5d197
RadagastW <radagastw@yandex.ru>
2025-02-28 16:07:21 +0500

    feat: implementation Behaviors class and related classes for update cycle

---
+++ Sharp.ConsoleApp/Commands/Behaviors/Jumper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sharp.ConsoleApp.Commands.Behaviors
+{
+    class Jumper : Behavior
+    {
+        public override void Update()
+        {
+            Console.WriteLine("Jumper");
+        }
+    }
+}
