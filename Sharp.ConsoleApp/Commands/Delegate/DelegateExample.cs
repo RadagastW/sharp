7ce49a0e8f3100cb0a155b4ae6107fd266aa0e40
RadagastW <radagastw@yandex.ru>
2025-06-10 10:17:04 +0500

    refactor: decompose DelegateExample class into smaller methods and add DemonstratePracticalUse method

--- Sharp.ConsoleApp/Commands/Delegate/DelegateExample.cs
+++ Sharp.ConsoleApp/Commands/Delegate/DelegateExample.cs
@@ -13,6 +13,17 @@ namespace Sharp.ConsoleApp.Commands.Delegate
         /// </summary>
         public void Execute()
         {
+            DemonstrateBasicDelegates();
+            DemonstrateMulticastDelegates();
+            DemonstrateDelegatesAsParameters();
+            DemonstrateReturningDelegates();
+            DemonstratePracticalUse();
+
+            WaitForUserInput();
+        }
+
+        private void DemonstrateBasicDelegates()
+        {
             Console.WriteLine("\"Пример работы с делегатами\"\n");
 
             Operation operation = MathHelper.Add;
@@ -20,20 +31,29 @@ namespace Sharp.ConsoleApp.Commands.Delegate
 
             operation = new Operation(MathHelper.Subtract); // альтернативный способ присвоения
             Console.WriteLine(operation?.Invoke(5, 6)); // альтернативный вызов делегата
+        }
 
+        private void DemonstrateMulticastDelegates()
+        {
             Console.WriteLine("\nОбъединение делегатов\n");
 
             OperationPrinter operationPrinter = MathHelper.PrintAddition;
             operationPrinter += MathHelper.PrintSubtraction;
             operationPrinter += MathHelper.PrintMultiplication;
             operationPrinter?.Invoke(5, 6);
+        }
 
+        private void DemonstrateDelegatesAsParameters()
+        {
             Console.WriteLine("\nДелегаты как параметры методов\n");
 
             MathHelper.DoOperation(7, 8, MathHelper.Add);
             MathHelper.DoOperation(7, 8, MathHelper.Subtract);
             MathHelper.DoOperation(7, 8, MathHelper.Multiply);
+        }
 
+        private void DemonstrateReturningDelegates()
+        {
             Console.WriteLine("\nВозвращение делегатов из метода\n");
 
             Operation SelectOperation(OperationType opType)
@@ -46,7 +66,7 @@ namespace Sharp.ConsoleApp.Commands.Delegate
                 }
             }
 
-            operation = SelectOperation(OperationType.Add);
+            Operation operation = SelectOperation(OperationType.Add);
             Console.WriteLine(operation(9, 10));
 
             operation = SelectOperation(OperationType.Subtract);
@@ -54,7 +74,38 @@ namespace Sharp.ConsoleApp.Commands.Delegate
 
             operation = SelectOperation(OperationType.Multiply);
             Console.WriteLine(operation(9, 10));
+        }
 
+        private void DemonstratePracticalUse()
+        {
+            Console.WriteLine("\nПрименение делегатов\n");
+
+            Account account = new Account(200);
+
+            account.RegisterHandler(PrintSimpleMessage);
+            account.RegisterHandler(PrintColorMessage);
+
+            account.Take(100);
+            account.Take(150);
+
+            account.UnregisterHandler(PrintColorMessage);
+            account.Take(50);
+        }
+
+        private void PrintSimpleMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        private void PrintColorMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        private void WaitForUserInput()
+        {
             Console.WriteLine("\nНажмите любую клавишу для продолжения...");
             Console.ReadKey();
             Console.WriteLine();
