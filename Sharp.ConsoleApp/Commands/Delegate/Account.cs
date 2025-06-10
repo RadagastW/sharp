76b4940fc112abfc65bc5571a39fedb1ca7cd0fb
RadagastW <radagastw@yandex.ru>
2025-06-10 10:14:55 +0500

    feat: implement Account class for practical delegate demonstration

---
+++ Sharp.ConsoleApp/Commands/Delegate/Account.cs
@@ -0,0 +1,46 @@
+﻿namespace Sharp.ConsoleApp.Commands.Delegate
+{
+    public delegate void AccountHandler(string message);
+
+    public class Account
+    {
+        private AccountHandler _taken;
+        private int _sum;
+
+        public Account(int sum)
+        {
+            _sum = sum;
+        }
+
+        // Регистрация делегата
+        public void RegisterHandler(AccountHandler del)
+        {
+            _taken += del;
+        }
+
+        // Отмена регистрации делегата
+        public void UnregisterHandler(AccountHandler del)
+        {
+            _taken -= del;
+        }
+
+        public void Add(int sum)
+        {
+            _sum += sum;
+        }
+
+        public void Take(int sum)
+        {
+            if (_sum >= sum)
+            {
+                _sum -= sum;
+
+                _taken?.Invoke($"Со счета списано {sum} у.е.");
+            }
+            else
+            {
+                _taken?.Invoke($"Недостаточно средств. Баланс: {_sum} у.е.");
+            }
+        }
+    }
+}
